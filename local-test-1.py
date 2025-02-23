2082b256ebd02c582e335e7944875079b12a57d2
sksn <s.skulsky@yandex.ru>
2025-02-23 11:30:29 +0500

    change

---
+++ local-test-1.py
@@ -0,0 +1,69 @@
+import time
+
+def print_large_digit(digit):
+    """
+    Prints the large digit passed as an argument.
+    
+    Args:
+        digit (int): The digit to be printed as a large character.
+    """
+    digit_patterns = [
+        [" *** ",
+         "*   *",
+         "*   *",
+         "*   *",
+         " *** "],
+        ["  *  ",
+         " **  ",
+         "  *  ",
+         "  *  ",
+         " *** "],
+        [" *** ",
+         "    *",
+         " *** ",
+         "*    ",
+         " *** "],
+        [" *** ",
+         "    *",
+         " *** ",
+         "    *",
+         " *** "],
+        ["*   *",
+         "*   *",
+         " *** ",
+         "    *",
+         "    *"],
+        [" *** ",
+         "*    ",
+         " *** ",
+         "    *",
+         " *** "],
+        [" *** ",
+         "*    ",
+         " *** ",
+         "*   *",
+         " *** "],
+        [" *** ",
+         "    *",
+         "    *",
+         "    *",
+         "    *"],
+        [" *** ",
+         "*   *",
+         " *** ",
+         "*   *",
+         " *** "],
+        [" *** ",
+         "*   *",
+         " *** ",
+         "    *",
+         " *** "]
+    ]
+
+    for line in digit_patterns[digit]:
+        print(line)
+
+for i in range(5, -1, -1):
+    print_large_digit(i)
+    time.sleep(1) 
+
